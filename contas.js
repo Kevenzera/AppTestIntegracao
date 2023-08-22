cdb6b7686ece6683780db2934ecadb05a5a40342
Keven Abraham <141691992+Kevenzera@users.noreply.github.com>
2023-08-22 01:02:16 +0000

    Update 4 files

--- contas.js
+++ contas.js
@@ -1,22 +1,23 @@
-function soma(a,b) {
-    return a + b;
-0}
-
-function subtracao(a,b) {
-    return a - b;
+function soma(a, b) {
+  return a + b;
+  0;
 }
 
-function divisao(a,b) {
-    return a / b;
+function subtracao(a, b) {
+  return a - b;
 }
 
-function multiplicacao(a,b) {
-    return a * b;
+function divisao(a, b) {
+  return a / b;
 }
 
-export {
-    soma,
-    subtracao,
-    multiplicacao,
-    divisao
+function multiplicacao(a, b) {
+  return a * b;
 }
+
+module.exports = {
+  soma,
+  subtracao,
+  divisao,
+  multiplicacao,
+};
