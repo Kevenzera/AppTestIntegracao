95fc39f196fa8541ae1aa0e7d79a9c5e1b13c7ea
Kevenzera <keven.souza@fatec.sp.gov.br>
2023-08-21 22:04:31 -0300

    enviando

--- contas.test.js
+++ contas.test.js
@@ -1,5 +1,17 @@
 const contas = require('/contas');
 
-test("Soma 2+5 esoerado 7 como resultado.", () => {
-    expect(contas.soma(2,5)).toBe(7);
-});
+test("Soma 2+5 esperado 7 como resultado.", () => {
+    expect(contas.soma(2,5)).toBe(7)
+})
+
+test("Multiplicação 10*10 esperado 100 como resultado", () => {
+    expect(contas.multiplicacao(10,10)).toBe(100)
+})
+
+test("Divisão 50/2 esperado 25 como resultado", () => {
+    expect(contas.divisao(50,2)).toBe(25)
+})
+
+test("Subtração 100-2 esperado 50 como resultado", () => {
+    expect(contas.subtracao(100,2)).toBe(98)
+})
